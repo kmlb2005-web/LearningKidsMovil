ec7ac6fc0033d4a4eb9c0196010c66b5692f6651
friiiAn <fridamo743@gmail.comm>
2026-05-02 10:58:56 -0600

    Mis cambios antes de actualizar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
-import Splash from "./features/home/screens/Splash";
+import React from 'react';
+import HomeScreen from "./HomeScreen2/CamposFormScreen";
 
 export default function Index() {
-  return <Splash />;
+  return <HomeScreen />;
 }
